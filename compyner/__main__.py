aa1f38c37cae6eadf4611c24b35cb4ef1035d8ed
GSG Robots <gsgrobots@gmail.com>
2025-01-29 11:12:29 +0100

    Add more options to compyner command

--- compyner/__main__.py
+++ compyner/__main__.py
@@ -14,11 +14,19 @@ def main():
     parser.add_argument("--output", "-o", required=False, action="store", type=FileType('w', encoding='utf-8'), default=sys.stdout)
     
     parser.add_argument("--exclude", required=False, action="store", type=str, default=None, nargs='+')
+    parser.add_argument("--split-modules", required=False, action="store_true", default=False)
+    parser.add_argument("--no-attrs", required=False, action="store_true", default=False)
     args = parser.parse_args()
     
     module_ast = ast.parse(args.input.read())
 
-    compyner = ComPYner(args.exclude)
+    compyner = ComPYner(
+        exclude=args.exclude,
+        debug_stack=False,
+        debug_line=False,
+        split_modules=args.split_modules,
+        use_attr=not args.no_attrs,
+        require_dunder_name=True,)
     compyner.add_module("__main__", module_ast)
     
     args.output.write(compyner.compyne())
